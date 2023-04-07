756770657d3a5d903bc65f8f16bc2c8d9d9bb7b7
chapcs <benchapman003@gmail.com>
2023-04-07 08:27:49 -0400

    modified ArrowDamage class

--- SwordDamage/Program.cs
+++ SwordDamage/Program.cs
@@ -37,58 +37,61 @@ internal class Program
 /// <summary>
 /// Main method that calculates the the sword damage based off different params
 /// </summary>
-class SwordDamage
+namespace SwordDamage
 {
-    public const int BASE_DAM = 3;
-    public const int FLAME_DAM = 2;
-
-    public int roll;
-    public bool flaming;
-    public bool magic;
-
-    public int Damage { get; private set; }
-
-    public SwordDamage(int startingRoll)
+    class SwordDamage
     {
-        roll = startingRoll;
-        CalculateDamage();
-    }
+        public const int BASE_DAM = 3;
+        public const int FLAME_DAM = 2;
 
-    public int Roll
-    {
-        get { return roll; }
-        set
+        public int roll;
+        public bool flaming;
+        public bool magic;
+
+        public int Damage { get; private set; }
+
+        public SwordDamage(int startingRoll)
         {
-            roll = value;
+            roll = startingRoll;
             CalculateDamage();
         }
-    }
 
-    private void CalculateDamage()
-    {
-        decimal MagicMult = 1M;
-        if (Magic) MagicMult = 1.75M;
-
-        Damage = (int)(Roll * MagicMult) + BASE_DAM;
-        if (Flaming) Damage += FLAME_DAM;
-        Debug.WriteLine($"CalculateDamage finished: {Damage} (roll: {Roll})");
-    }
-    public bool Magic
-    {
-        get { return magic; }
-        set
+        public int Roll
         {
-            magic = value;
-            CalculateDamage();
+            get { return roll; }
+            set
+            {
+                roll = value;
+                CalculateDamage();
+            }
         }
-    }
-    public bool Flaming
-    {
-        get { return flaming; }
-        set
+
+        private void CalculateDamage()
         {
-            flaming = value;
-            CalculateDamage();
+            decimal MagicMult = 1M;
+            if (Magic) MagicMult = 1.75M;
+
+            Damage = (int)(Roll * MagicMult) + BASE_DAM;
+            if (Flaming) Damage += FLAME_DAM;
+            Debug.WriteLine($"CalculateDamage finished: {Damage} (roll: {Roll})");
+        }
+        public bool Magic
+        {
+            get { return magic; }
+            set
+            {
+                magic = value;
+                CalculateDamage();
+            }
+        }
+        public bool Flaming
+        {
+            get { return flaming; }
+            set
+            {
+                flaming = value;
+                CalculateDamage();
+            }
         }
     }
 }
